436b5d6623cdac3f47ae1784aaa93c697581d65b
RootPacman <75959846+RootPacman@users.noreply.github.com>
2021-02-05 07:02:34 +0700

    Add files via upload

---
+++ dorking.py
@@ -0,0 +1,29 @@
+import os
+import time
+
+os.system('clear')
+#Pacman
+def U():
+#Gans
+
+	print("Wait A Second,Tools By r$0t_p4c$m4n")
+	time.sleep(2)
+	print("")
+	print("Note : Tools Belum Sempurna,Akan Error Jika Kebanyakan Web :D")
+	time.sleep(2)
+	print("")
+	print('How To Dorking ? : ("author/admin") (ini salah,tools akan error)')
+	print("")
+	time.sleep(2)
+	print('How To Dorking? : ("author/admin") intext:"Powered By Wordpress" 2021 (Ini Benar)')
+	print("")
+	time.sleep(1)
+	print('Catatan : Ga Harus Website Wordpress,Yang Penting Dork nya ga pendek amat,kalo bisa agak panjangan biar ga error hehe')
+	print("")
+	time.sleep(2)
+U()
+from googlesearch import search
+query = str(input("Keyword: "))
+os.system('clear')
+for URL in search(query):
+  print(URL)
